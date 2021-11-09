d3551e31f221ca1ba6fb88413b0bdf7fd240b0df
mertaksu28 <mertaksu28@outlook.com.tr>
2021-11-10 01:41:19 +0300

    DataTypes

---
+++ dataTypes.ts
@@ -0,0 +1,38 @@
+let sayi: number = 12
+sayi = 10
+sayi = 10.4
+
+let sehir: string = "Ankara"
+sehir = "Giresun"
+sehir = "İstanbul"
+
+let isTrue: boolean = true
+isTrue = true
+isTrue = false
+
+let sayilar: number[] = [1, 2, 3, 4, 5]
+
+let sayilar2: Array<number> = [1, 2, 3]
+
+//Tuple
+let dizi: [number, string] = [1, "Merhaba"]
+dizi[0]
+dizi[1]
+
+enum Renk { Kirmizi = 1, Siyah, Mavi }
+
+let renk: Renk = Renk.Mavi
+
+//Any: Bize gelecek olan Data'nın veri tipinden emin olmadığımız zaman kullanırız.
+let deger: any = "Ankara"
+deger = 10
+deger = {}
+
+//Void: Genellikle fonksiyonlarda kullanırız. Bir değer döndürmek istemediğimiz zaman kullanırız.
+
+//Undefined
+let yas:number //Undefined
+
+//Null: Referans Tiplerde kullanılır. Yani referansı oluşmamış demek Null demek
+
+
