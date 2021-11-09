b6d432e17aeebe59cd6d8aaa2c0be1c2af76a211
mertaksu28 <mertaksu28@outlook.com.tr>
2021-11-10 01:01:31 +0300

    firstcommit

---
+++ intro.js
@@ -0,0 +1,5 @@
+function selamVer(isim) {
+    return "Merhaba " + isim;
+}
+var mesaj = selamVer("Mert");
+console.log(mesaj);
---
+++ intro.ts
@@ -0,0 +1,7 @@
+function selamVer(isim:string) {
+    return "Merhaba " + isim
+}
+
+let mesaj = selamVer("Mert")
+
+console.log(mesaj)
